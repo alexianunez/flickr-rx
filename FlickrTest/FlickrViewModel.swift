60e8b67821202b1913bc597aa1faed50192eff0f
Alexia Nunez <alexia.nunez@lifelock.com>
2016-08-31 11:18:07 -0700

    Almost-working parser

--- FlickrTest/FlickrViewModel.swift
+++ FlickrTest/FlickrViewModel.swift
@@ -11,6 +11,7 @@ import RxCocoa
 import RxSwift
 
 enum FlickrAPIResponseKeys: String {
+    case Photos = "photos"
     case Id = "id"
     case FarmId = "farm"
     case ServerID = "server"
@@ -42,22 +43,65 @@ struct FlickrViewModel {
     }()
     
     func getPhotos(searchTerm: String) -> Observable<[Photo]> {
+        
+        guard !searchTerm.isEmpty,
+            let url = NSURL(string: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d0e947f859d1f9d93c5a5c6e40e4670a&format=json&tags=\(searchTerm)")
+            else {
+                return Observable.just([])
+        }
+        
+        return NSURLSession.sharedSession()
+            
+            .rx_data(NSURLRequest(URL: url))
+                
+            .retry(3)
+        
+            .map { data in
+                
+                let photos: [Photo] = [Photo]()
+                
+                guard let
+                    result = NSString(data: data, encoding: NSUTF8StringEncoding) as? String,
+                    dict = self.convertStringToDictionary(result),
+                    items = dict[FlickrAPIResponseKeys.Photos.rawValue]
+                    else {
+                        return photos
+                }
+                
+                
+                print("\(items)")
 
-        return Observable.empty()
-        
-//        guard !searchTerm.isEmpty,
-//            let url = NSURL(string: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d0e947f859d1f9d93c5a5c6e40e4670a&format=json&tags=\(searchTerm)")
-//            else { return Observable.just([]) }
-//        
-//        return NSURLSession.sharedSession()
-        
-            
-        
-        
-            
-        
- 
+                
+             
+                
+                
+                return photos
+                
+                
+        }
         
     }
+    
+    func convertStringToDictionary(text: String) -> [String:AnyObject]? {
+        
+        var cleanString = text.stringByReplacingOccurrencesOfString("jsonFlickrApi(", withString: "")
+        
+        cleanString = cleanString.substringToIndex(cleanString.endIndex.predecessor())
+        
+        if let data = cleanString.dataUsingEncoding(NSUTF8StringEncoding) {
+            
+            do {
+                
+                return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String:AnyObject]
+                
+            } catch {
+                
+                return nil
+            }
+        }
+        
+        return nil
+    }
+
     
 }
